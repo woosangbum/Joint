156ff899165d0aeb5025525688a739669f901d1e
pug9483 <thswlals0219@gmail.com>
2021-11-21 20:16:09 +0900

    test

--- app/src/main/java/com/example/joint/NoticeListActivity.java
+++ app/src/main/java/com/example/joint/NoticeListActivity.java
@@ -2,17 +2,116 @@ package com.example.joint;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class NoticeListActivity extends AppCompatActivity {
     // 공시사항 리스트
+    private String id;
+    private String title;
+    private String content;
+    private String date;
+
+    private ListView notice_view;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> arrayList = new ArrayList<String>();
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice_list);
+
+        notice_view = (ListView) findViewById(R.id.noticeView);
+        Log.d("dkdkdkkdkdkdkdkkd", "key: ");
+        Log.e("dkdkdkkdkdkdkdkkd", "key: ");
+        System.out.println("hello");
+        showNoticeList();
+//        showNoticeList2();
     }
+
+    private void showNoticeList2() {
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        notice_view.setAdapter(adapter);
+        notice_view.setTranscriptMode(ListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+
+        // 데이터 받아오기 및 어댑터 데이터 추가 및 삭제 등..리스너 관리
+        databaseReference.child("notice_list").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Log.d("MainActivity", "ValueEventListener : " + snapshot.getValue());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+    }
+
+    private void showNoticeList() {
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        notice_view.setAdapter(adapter);
+        notice_view.setTranscriptMode(ListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+
+        // 데이터 받아오기 및 어댑터 데이터 추가 및 삭제 등..리스너 관리
+        databaseReference.child("notice_list").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Log.e("LOG", "dataSnapshot.getKey() : " + dataSnapshot.getKey());
+                adapter.add(dataSnapshot.getKey());
+
+                Log.e("getFirebaseDatabase", "key: " + dataSnapshot.getChildrenCount());
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    String key = postSnapshot.getKey();
+                    Log.d("getFirebaseDatabase", "key: " + key);
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+
+
+
     public void onClickNotice(View v){
         Intent intent = new Intent(NoticeListActivity.this, NoticeActivity.class);
         startActivity(intent);
